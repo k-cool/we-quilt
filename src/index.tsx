9a58693262485933d266686100537f0a3064daaa
wook95 <uclee95@gmail.com>
2021-12-22 15:18:05 +0900

    UPDATE: provider를 index에서 app으로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
