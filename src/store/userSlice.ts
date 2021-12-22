a57c28424cf9a9441df539520784f5836418b4b5
wook95 <uclee95@gmail.com>
2021-12-22 14:57:57 +0900

    ADD: redux 초기셋팅

---
+++ src/store/userSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLogin: false,
+  userInfo: { name: '' },
+  accessToken: '',
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginAction: (state, { payload }) => {
+      state.isLogin = true;
+      state.userInfo = payload.userInfo || {
+        name: '유저 이름이 입력되지 않았습니다.',
+      };
+      state.accessToken = payload.accessToken || '';
+    },
+    logoutAction: () => {
+      return initialState;
+    },
+    editUserInfoAction: (state, { payload }) => {
+      state.userInfo = { ...state.userInfo, ...payload.userInfo };
+    },
+  },
+});
+
+export const { loginAction, logoutAction, editUserInfoAction } = userSlice.actions;
+export default userSlice.reducer;
